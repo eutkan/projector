557c7749e4c1b5fda9a3d7afdcd28ce0fe838f80
Elifnaz Utkan <elifnazutkan@gmail.com>
2018-05-17 17:33:46 +0300

    Use root user for mysql connection by default

--- db/connection.py
+++ db/connection.py
@@ -3,7 +3,7 @@ import pymysql.cursors
 connection = pymysql.connect(
     host='127.0.0.1',
     db='projector',
-    user='projector',
+    user='root',
     password='',
     cursorclass=pymysql.cursors.DictCursor
 )
